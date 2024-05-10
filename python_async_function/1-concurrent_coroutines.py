d09da9b8480d00dce5b295dca905e488bba643eb
Gasorekibo <m.gasore@alustudent.com>
2024-05-10 19:25:35 +0200

    fix the bug

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-
 """
-    This module contains an async routine called wait_n,
-    it takes in 2 int arguments n, and max_delay,
-    it will spawn wait_random n times with the specified max_delay.
-    wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending
-    order without using sort() because of concurrency
+Execute multiple coroutines at the same time
 """
-
 import asyncio
 from typing import List
+from asyncio import gather
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-        This async routine takes in two int arguments and
-        returns a list of all the delays
+    A function that executes multiple coroutines by calling 'wait_random',
+    Args:
+        n -> number of times to spawn 'wait_random'
+        max_delay -> the number of seconds to wait before returning output
     """
+    tasks = [wait_random(max_delay) for x in range(n)]
+    data_list = await gather(*tasks)
+    new_list = []
 
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    result = await asyncio.gather(*tasks)
-    return sorted(result)
+    while data_list:
+        minimum = data_list[0]  # arbitrary number in list
+        for x in data_list:
+            if x < minimum:
+                minimum = x
+        new_list.append(minimum)
+        data_list.remove(minimum)
+
+    return new_list
+
+
+if __name__ == "__main__":
+    asyncio.run(wait_n)
