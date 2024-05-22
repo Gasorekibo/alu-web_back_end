3f2bd472654e7f9688f4e36268af1e0bf5b96899
Gasorekibo <m.gasore@alustudent.com>
2024-05-22 22:44:02 +0200

    filter

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Filter datum logger
+"""
+
+import re
+from typing import List
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """
+    Returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
