594fca5cb27a27c9c5d36d4abece31eb0353575b
Gasorekibo <m.gasore@alustudent.com>
2024-07-04 13:21:15 +0200

    mysql database question 1

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Create Database and Insert initial Data
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user (
     id int AUTOINCREMENT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
