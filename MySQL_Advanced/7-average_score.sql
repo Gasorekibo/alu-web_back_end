f002c773c2ba5bb50167d16435ade6985b984f47
Gasorekibo <m.gasore@alustudent.com>
2024-07-04 13:53:32 +0200

    trigger for question 7

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -2,17 +2,17 @@
 -- Note: An average score can be a decimal
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN input_user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
 
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = input_user_id;
 
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id;
+    WHERE id = input_user_id;
 END //
 
 DELIMITER ;
