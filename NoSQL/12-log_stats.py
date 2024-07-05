e58a6bbbb2ad397f248f065a747d5be05c16ab96
Gasorekibo <m.gasore@alustudent.com>
2024-07-05 23:31:10 +0200

    correct naming

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-""" 
-    MongoDB Operations with pymongo
-"""
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
+    """ Provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    number_of_logs = nginx_collection.count_documents({})
-    print(f'{number_of_logs} logs')
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
@@ -17,8 +16,8 @@ if __name__ == "__main__":
         count = nginx_collection.count_documents({"method": method})
         print(f'\tmethod {method}: {count}')
 
-    status_checking = nginx_collection.count_documents(
+    status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
 
-    print(f'{status_checking} status check')
+    print(f'{status_check} status check')
