cfbaf77754cc3c34af83c51d382fc085bca2f464
Gasorekibo <m.gasore@alustudent.com>
2024-07-06 23:16:22 +0200

    dealing with documentation checker

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 from pymongo import MongoClient
 """ pythong with pymongo"""
-
 client = MongoClient('localhost', 27017)
+
+# Access the 'logs' database and 'nginx' collection
 db = client.logs
 collection = db.nginx
-total_logs = collection.count_documents({})
 
+# Count the total number of documents in the 'nginx' collection
+total_logs = collection.count_documents({})
 print(f"{total_logs} logs")
 
-# Count methods: GET, POST, PUT, PATCH, DELETE
+# Define HTTP methods to count
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+# Count the number of documents for each HTTP method
 method_counts = {method: collection.count_documents({"method": method}) for method in methods}
 
 print("Methods:")
+"""Print the counts for each HTTP method"""
 for method, count in method_counts.items():
     print(f"    method {method}: {count}")
+
+
 status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+"""Count the number of GET requests to the /status path"""
 print(f"{status_check_count} status check")
 
 client.close()
