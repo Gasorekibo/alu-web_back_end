a61b38099750b60632f80b95c4572920beec2657
Gasorekibo <m.gasore@alustudent.com>
2024-07-07 00:11:21 +0200

    calling a function

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,6 @@
 function taskFirst() {
-  const task = "I prefer const when I can";
-  let changeValue = "But sometimes let";
+  const task = 'I prefer const when I can';
+  let changeValue = 'But sometimes let';
   return `${task} ${changeValue}`;
 }
+taskFirst()
