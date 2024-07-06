966c8fd6871bfffd079720af4ab595223bb6c400
Gasorekibo <m.gasore@alustudent.com>
2024-07-07 00:09:15 +0200

    to pass lint

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
-function taskFirst(){
-    const task = "I prefer const when I can"
-    let changeValue = "But sometimes let"
-    return `${task} ${changeValue}`
+function taskFirst() {
+  const task = "I prefer const when I can";
+  let changeValue = "But sometimes let";
+  return `${task} ${changeValue}`;
 }
