1ccb769325f644fe77b028d0aef2520a2d783077
Gasorekibo <m.gasore@alustudent.com>
2024-07-12 19:55:22 +0200

    handling promise with thenable

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,15 @@
-handleResponseFromAPI(promise)
-  .then(() => {
-    status: 200;
-    body: 'success';
-  })
-  .catch(() => new Error())
-  .finally(() => console.log('Got a response from the API'));
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return {
+        status: 200,
+        body: "success",
+      };
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log("Got a response from the API");
+    });
+}
