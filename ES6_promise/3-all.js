60adefaccb64b2669de0ee2c804c9b04249836f5
Gasorekibo <m.gasore@alustudent.com>
2024-07-12 20:24:59 +0200

    promise combinator all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,5 +1,5 @@
-import uploadPhoto  from './utils';
-import uploadPhoto  from './utils';
+import {uploadPhoto}  from './utils';
+import {uploadPhoto}  from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
