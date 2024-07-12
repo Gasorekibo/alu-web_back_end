0f03a673d86fbf62261beaa018df3f414492a0d2
Gasorekibo <m.gasore@alustudent.com>
2024-07-12 20:12:51 +0200

    promise combinator all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils.js';
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].body);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
