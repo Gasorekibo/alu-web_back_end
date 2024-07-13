b57506eaa07dc3e12bece2b4b4c8ba593f4a51f1
Gasorekibo <m.gasore@alustudent.com>
2024-07-13 18:08:39 +0200

    advanced

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,19 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-
-    const upload = await uploadPhoto();
-    const user = await createUser();
-    if (upload && user) {
-      return {
-        photo: upload,
-        user,
-      };
-    } else {
-        return {
-            photo: null,
-            user: null,
-        };
-    }
-  
+  const upload = await uploadPhoto();
+  const user = await createUser();
+  if (upload && user) {
+    return {
+      photo: upload,
+      user,
+    };
+  } else {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
