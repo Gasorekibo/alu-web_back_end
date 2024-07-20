d73a352e4c862b04db4a859d39c58c2d8f34691b
Gasorekibo <m.gasore@alustudent.com>
2024-07-20 20:45:13 +0200

    move initialization after declaration of class

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -25,11 +25,11 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
@@ -42,10 +42,11 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export default listOfStudents = [
+ const listOfStudents = [
   student1,
   student2,
   student3,
   student4,
   student5,
 ];
+export default listOfStudents
