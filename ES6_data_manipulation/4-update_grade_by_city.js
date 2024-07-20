28ccc7136a67d10b13f3d62396443e3893826a3f
Gasorekibo <m.gasore@alustudent.com>
2024-07-20 22:23:13 +0200

    update student grade by city

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,10 +6,32 @@ export default function updateStudentGradeByCity(arr, city, newGrades) {
   return students.map((each) => {
     newGrades.filter((grad) => {
       if (each.id === grad.studentId) {
-        each.grade = grad.grade || 'N/A';
+        each.grade = grad.grade;
+      } else {
+        each.grade = "N/A";
       }
     });
 
     return each;
   });
 }
+
+const obj = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+
+  console.log(updateStudentGradeByCity(obj,"San Francisco", [{ studentId: 5, grade: 97 }]))
