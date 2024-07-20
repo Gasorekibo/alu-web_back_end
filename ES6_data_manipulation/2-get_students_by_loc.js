fb90c63d3de52ada177faed456f1c6f98513511c
Gasorekibo <m.gasore@alustudent.com>
2024-07-20 21:38:57 +0200

    filter student by city

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(arr, city) {
   return Array.isArray(arr)
-    ? arr.filter((student) => student.city === city)
+    ? arr.filter((student) => student.location === city)
     : [];
 }
