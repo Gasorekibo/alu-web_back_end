a97436a7a70ceb8e5ebcc5dcf72823857a199742
Gasorekibo <m.gasore@alustudent.com>
2024-08-02 15:06:04 +0200

    solve to pass lint

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 
-const readDb = async (path) => new Promise((resolve, reject) => {fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
-  if (err) {
-    reject(new Error('Cannot load the database'));
-    return;
-  }
-  resolve(data);
+const readDb = async (path) => new Promise((resolve, reject) => { 
+  fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    resolve(data);
   });
   });
 
